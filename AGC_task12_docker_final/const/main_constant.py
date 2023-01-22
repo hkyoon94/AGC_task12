fe9834a5a25f470f18d0dd8c23e40c2666949097
윤홍규(Hong-Gyu Yoon) <83046925+hkyoon94@users.noreply.github.com>
2023-01-22 17:29:30 +0900

    Delete AGC_task12_docker_final directory

--- AGC_task12_docker_final/const/main_constant.py
+++
@@ -1,2 +0,0 @@
-data_path = '/home/hgyoon/Research/AGC/task12_docker_final/dataset'
-doc_path = '/home/hgyoon/Research/AGC/docs_filtered_json_v4'
--- AGC_task12_docker_final/const/task12_constant.py
+++
@@ -1,3 +0,0 @@
-encoder_path = 'jhgan/ko-sroberta-multitask'
-mrc_model_path = '/home/hgyoon/Research/AGC/gov_120000'
-is_local = False
